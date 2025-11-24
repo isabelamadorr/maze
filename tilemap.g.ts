65526154fd66171fd593783f24709ef3ac5d4068
jimenafuentes-cloud <jimena.fuentes@panam.ed.cr>
2025-11-24 10:17:03 -0600

    Sprite and walls

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -26,22 +26,22 @@ namespace myTiles {
 `, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,myTiles.transparency16], TileScale.Sixteen);
             case "level_1":
             case "level_1":return tiles.createTilemap(hex`100010000102020c0c1301020c0202130c0202030a0e0b0b0b0b090b0b0b0b0b0b0b0e12110b0b0b0b0b090b0b0b0b0e0b0b0b0d090b0b010c0c160e0b1507070f0b0b0d0a0b0b090b0b0b0b0b0d0e0b090b0b04090e0b0a0b0b0e0b0b0d0b0b0a0b0b04100b0b0a0b0b090e0b0d0b0e090e0b120a0b0b080f0e090b0b0d0e0b100b0b0d090b0e0b0806090b0b0d0b0b0a0b0b040a0b0b0b0e0b090e0b0d0b0b090e0b0d0a0b0b0b0b0b090b0b0d0b0b090b0b04090b0b0c0c0c160b0e0b0b0e090b0b04100b0b0b0b0b0e0b0b0b0b0b090b0b120a0b0b0b0b0b090b0b0b0b0b0a0b1704090e0b0b0b0e090b0b0e0b0b090e0b0408070714070608060714060608060705`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . . . . . . . 2 
+. . . . . . 2 . . . . . . . . 2 
+2 . . 2 2 2 2 . . 2 2 2 2 . . 2 
+2 . . 2 . . . . . 2 . . 2 . . 2 
+2 . . 2 . . . . . 2 . . 2 . . 2 
+2 . . 2 . . 2 . . 2 . . 2 . . 2 
+2 . . 2 2 . 2 . . 2 . . 2 . . 2 
+2 . . . 2 2 2 . . 2 . . 2 . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 
+2 . . 2 2 2 2 . . . . . 2 . . 2 
+2 . . . . . . . . . . . 2 . . 2 
+2 . . . . . 2 . . . . . 2 . . 2 
+2 . . . . . 2 . . . . . 2 . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest2,sprites.dungeon.stairWest,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.stairLadder], TileScale.Sixteen);
         }
         return null;
